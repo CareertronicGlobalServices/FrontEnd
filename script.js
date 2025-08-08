9c727755c26c303923359cfca280bd5766d65e64
Sonam <sonam.panjwani29@gmail.com>
2025-08-08 14:41:10 +0530

    First JS

---
+++ script.js
@@ -0,0 +1,38 @@
+//console.log("Hie Shubham");
+//Variable --vary(change) able (ability)
+//aisa koi value jo pure program me change ho sakta hai
+//Constant - kabhi change ni hoga
+// Valur has a type but not a variable // dynamic typing -
+
+let a = 5;
+let b = "KAnchan";
+let c = 125356874145;
+let d = false;
+
+//console.log(typeof ab);
+
+// d = true;
+
+// console.log(d);
+
+//Const
+//constant hum nahi badal sakte
+var ab = 10;
+//console.log(a);
+//a = 15;
+//console.log(ab);
+//Non Primitive Data Types
+// let arry = [1, 2, 4, 6];
+// console.log(typeof arry); // remember this
+// console.log(arry[1]);// array[index] //to access its elements
+
+//objects
+let objShubham = {
+  name: "Shubham",
+  age: "25",
+  emailID: "shubham@careertronic.com",
+  address: "Raipur",
+  attendance: true,
+};
+console.log(typeof objShubham);
+console.log(typeof objShubham.emailID);
