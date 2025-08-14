4d13205c8579df347b433d8c60d9d94a3aeeb932
Sonam <sonam.panjwani29@gmail.com>
2025-08-14 14:49:13 +0530

    switch case

---
+++ switch.js
@@ -0,0 +1,40 @@
+let day = "-";
+let a = 7;
+let b = 90;
+
+switch (day) {
+  case "+": {
+    console.log("Addition");
+
+    var sum = b - a;
+    console.log(sum);
+  }
+  case "-": {
+    console.log("Tuesday");
+    console.log(sum);
+    break;
+  }
+  case 3: {
+    console.log("wednesday");
+    break;
+  }
+  case 4: {
+    console.log("thursday");
+    break;
+  }
+  case 5: {
+    console.log("friday");
+    break;
+  }
+  case 6: {
+    console.log("saturday");
+    break;
+  }
+  case 7: {
+    console.log("sunday");
+    break;
+  }
+  default: {
+    console.log("Entr value from 1 to 7 ");
+  }
+}
