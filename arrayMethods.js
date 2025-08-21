c69c179dfbc56930948c242f468c56fc03e502b5
Sonam <sonam.panjwani29@gmail.com>
2025-08-21 14:44:15 +0530

    array methods 2

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,5 +1,5 @@
-let a = [1, 2, 3, 4, 5];
-let b = [6, 7, 8, 9, 10];
+// let a = [1, 2, 3, 4, 5];
+// let b = [6, 7, 8, 9, 10];
 
 //Array method 1 push
 // a.push(11); //1. add to the last of the array 2. returns the same updated array
@@ -38,7 +38,36 @@ let b = [6, 7, 8, 9, 10];
 // console.log(a); //does not update the original array
 
 //method 9: Splice
-console.log(a); //original array
-let k = a.splice(2, 1); //starting index , kitne element udane hai
-console.log(k); //new array of the spliced elements
-console.log(a); //manipulation of  the original array
+// console.log(a); //original array
+// let k = a.splice(2, 1); //starting index , kitne element udane hai
+// console.log(k); //new array of the spliced elements
+// console.log(a); //manipulation of  the original array
+
+// array methods
+// let a = ["Shubham ", "Gulshan", "Lavish"];
+// let b = ["Sonam ", "Shreyansh", "Aditya"];
+// let c =a.push(b);
+// console.log(c);
+//Concat
+// let c = a.concat(b);
+// console.log(c);
+
+//-flat
+let a = [1, 3, 2, 4, 5, [1, [4, 5, [1, 2, 4]]]];
+let b = [1, 2, 3];
+let c = [4, 5, 6];
+let kd = [...b]; //concat /b...
+console.log(kd);
+
+// let b = a.flat(Infinity); // flat takes the input as depth
+// console.log(a);
+// console.log(b);
+// //check the syntsx
+// console.log(Array.isArray(b)); // true /false
+// console.log(typeof Array); //constructor function
+// //make an array from string
+// let c = Array.from("89"); //String
+// console.log(c);
+
+// let d = Array.of("Sonam ", "Shubham ", "Gulshan");
+// console.log(d);
