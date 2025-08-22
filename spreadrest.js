541739f805132c6a9268a439b252c2050d021fb3
Sonam <sonam.panjwani29@gmail.com>
2025-08-22 14:46:42 +0530

    spreadRest

---
+++ spreadrest.js
@@ -0,0 +1,20 @@
+//
+const newObject = {
+  name: "Sonam",
+  age: 32,
+};
+console.log(newObject);
+newObject.college = "JIT";
+console.log(newObject);
+newObject.name = "Kanchan";
+console.log(newObject);
+
+// newObject = {
+//   name: "shubham",
+// };
+// console.log(newObject);
+
+const a = 5;
+console.log(a);
+a = 10;
+console.log(a);
